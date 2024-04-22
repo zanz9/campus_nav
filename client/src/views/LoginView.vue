fb9dee157df03cbcf20a225f830728d3d424ff58
ernur torekul <ernurtorekul@gmail.com>
2024-04-22 17:25:39 +0500

    login 2

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -6,13 +6,16 @@ import { ref } from 'vue'
 </script>
 
 <template>
-
-    <div>
-      <v-img
+  <v-container>
+    <v-row>
+      <v-col align-self="center">
+        <div>
+      
+      <!-- <v-img
         class="mx-auto my-6"
         max-width="228"
-        src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
-      ></v-img>
+        <a fds>
+      </v-img> -->
   
       <v-card
         class="mx-auto pa-12 pb-8"
@@ -20,17 +23,17 @@ import { ref } from 'vue'
         max-width="448"
         rounded="lg"
       >
-        <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+        <div class="text-subtitle-1 text-medium-emphasis">Почта</div>
   
         <v-text-field
           density="compact"
-          placeholder="Email address"
+          placeholder="Адрес электронной почты"
           prepend-inner-icon="mdi-email-outline"
           variant="outlined"
         ></v-text-field>
   
         <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-          Password
+          Пароль
   
           <a
             class="text-caption text-decoration-none text-blue"
@@ -38,14 +41,14 @@ import { ref } from 'vue'
             rel="noopener noreferrer"
             target="_blank"
           >
-            Forgot login password?</a>
+          Забыли пароль для входа в систему?</a>
         </div>
   
         <v-text-field
           :append-inner-icon="visible ? mdiEyeOff : mdiEye"
           :type="visible ? 'text' : 'password'"
           density="compact"
-          placeholder="Enter your password"
+          placeholder="Введите свой пароль"
           prepend-inner-icon="mdi-lock-outline"
           variant="outlined"
           @click:append-inner="visible = !visible"
@@ -56,9 +59,6 @@ import { ref } from 'vue'
           color="surface-variant"
           variant="tonal"
         >
-          <v-card-text class="text-medium-emphasis text-caption">
-            Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for three hours. If you must login now, you can also click "Forgot login password?" below to reset the login password.
-          </v-card-text>
         </v-card>
   
         <v-btn
@@ -68,7 +68,7 @@ import { ref } from 'vue'
           variant="tonal"
           block
         >
-          Log In
+          Войти
         </v-btn>
   
         <v-card-text class="text-center">
@@ -78,11 +78,15 @@ import { ref } from 'vue'
             rel="noopener noreferrer"
             target="_blank"
           >
-            Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+          Зарегистрируйтесь прямо сейчас <v-icon icon="mdi-chevron-right"></v-icon>
           </a>
         </v-card-text>
       </v-card>
     </div>
+      </v-col>
+    </v-row>
+  </v-container>
+    
   </template>
   <script>
     export default {
