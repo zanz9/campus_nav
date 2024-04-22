7f121d84218671b63700e7280d31a1daa7ddef29
ernur torekul <ernurtorekul@gmail.com>
2024-04-22 16:26:07 +0500

    added login

--- client/src/main.js
+++ client/src/main.js
@@ -5,10 +5,21 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import { mdiAccount } from '@mdi/js'
 
 const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases: {
+          ...aliases,
+        },
+        sets: {
+          mdi,
+        },
+      },
 })
 
 
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -13,6 +14,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
     }
   ]
 })
